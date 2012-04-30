3f59ced7001d456762be37dfc26f23ac10bf33c2
efrohnho <efrohnho@gmail.com>
2012-04-29 20:49:46 -0400

    Removed duplicate files

--- spatialite-android-library/jni/Application.mk
+++ spatialite-android-library/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS += -frtti
 APP_CPPFLAGS += -fexceptions
-APP_ABI := armeabi armeabi-v7a x86
+#APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := x86
--- spatialite-android-library/jni/iconv.mk
+++ spatialite-android-library/jni/iconv.mk
@@ -2,18 +2,18 @@ include $(CLEAR_VARS)
 # ./configure --build=x86_64-pc-linux-gnu --host=arm-linux-eabi
 LOCAL_MODULE := iconv
 LOCAL_CFLAGS := \
-	-Wno-multichar \
-	-D_ANDROID \
-	-DBUILDING_LIBICONV \
-	-DIN_LIBRARY \
-	-DLIBDIR="\"~/android-libs/usr/local/lib\"" 
+        -Wno-multichar \
+        -D_ANDROID \
+        -DBUILDING_LIBICONV \
+        -DIN_LIBRARY \
+        -DLIBDIR="\"~/android-libs/usr/local/lib\"" 
 LOCAL_C_INCLUDES := \
-	$(ICONV_PATH)/libiconv-1.13.1 \
-	$(ICONV_PATH)/libiconv-1.13.1/include \
-	$(ICONV_PATH)/libiconv-1.13.1/lib \
-	$(ICONV_PATH)/libiconv-1.13.1/libcharset/include
+        $(ICONV_PATH) \
+        $(ICONV_PATH)/include \
+        $(ICONV_PATH)/lib \
+        $(ICONV_PATH)/libcharset/include
 LOCAL_SRC_FILES := \
-	$(ICONV_PATH)/lib/iconv.c \
-	$(ICONV_PATH)/lib/relocatable.c \
-	$(ICONV_PATH)/libcharset/lib/localcharset.c
+        $(ICONV_PATH)/lib/iconv.c \
+        $(ICONV_PATH)/lib/relocatable.c \
+        $(ICONV_PATH)/libcharset/lib/localcharset.c
 include $(BUILD_STATIC_LIBRARY)
