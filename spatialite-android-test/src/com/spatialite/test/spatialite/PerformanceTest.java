2bfda964a2163b289341c5f50ea9aae1a95c7722
efrohnho <efrohnho@gmail.com>
2012-04-29 21:36:43 -0400

    Removed warning Removed empty.cpp files Re-enabled all build targets

--- spatialite-android-test/src/com/spatialite/test/spatialite/PerformanceTest.java
+++ spatialite-android-test/src/com/spatialite/test/spatialite/PerformanceTest.java
@@ -2,8 +2,6 @@ package com.spatialite.test.spatialite;
 
 import com.spatialite.test.utilities.DatabaseTestCase;
 
-import android.test.AndroidTestCase;
-
 public class PerformanceTest extends DatabaseTestCase {
 
 }
