2147908dc63b773851c37f65746340ce8c76533d
Theeban Soundararajan <theeban@intersect.org.au>
2012-12-14 13:08:09 +1100

    Adding missing changes

--- faims-android-app/src/au/org/intersect/faims/android/tasks/TaskTypes.java
+++
@@ -1,6 +0,0 @@
-package au.org.intersect.faims.android.tasks;
-
-public enum TaskTypes {
-	FETCH_PROJECTS_LIST,
-	DOWNLOAD_PROJECT
-}
--- faims-android-app/src/au/org/intersect/faims/android/ui/dialog/DialogResultCodes.java
+++
@@ -1,6 +0,0 @@
-package au.org.intersect.faims.android.ui.dialog;
-
-public enum DialogResultCodes {
-	SUCCESS, FAILURE, SELECT_YES, SELECT_NO, SELECT_OK
-
-}
