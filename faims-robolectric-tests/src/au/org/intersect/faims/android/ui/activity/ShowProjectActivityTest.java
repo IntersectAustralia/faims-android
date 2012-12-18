00943588317c4237e7b4fcfca6db7d1ea538d805
Theeban Soundararajan <theeban@intersect.org.au>
2012-12-18 16:59:39 +1100

    Adding dummy test

--- faims-robolectric-tests/src/au/org/intersect/faims/android/ui/activity/ShowProjectActivityTest.java
+++ faims-robolectric-tests/src/au/org/intersect/faims/android/ui/activity/ShowProjectActivityTest.java
@@ -1,5 +1,16 @@
 package au.org.intersect.faims.android.ui.activity;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import au.org.intersect.faims.android.roblectric.FAIMSRobolectricTestRunner;
+
+@RunWith(FAIMSRobolectricTestRunner.class)
 public class ShowProjectActivityTest {
 
+	@Test
+	public void dummytest() {
+		
+	}
+	
 }
