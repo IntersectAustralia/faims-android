8ad9f9607a89662131542b5f92888a5c4190160f
Chris Kenward <chrisk@intersect.org.au>
2013-01-14 09:48:40 +1100

    FAIMS-113 Provide programmatic back button functionality to be used by cancel

--- faims-android-app/src/au/org/intersect/faims/android/util/BeanShellLinker.java
+++ faims-android-app/src/au/org/intersect/faims/android/util/BeanShellLinker.java
@@ -208,6 +208,10 @@ public class BeanShellLinker {
 		}
 	}
 
+	public void goBack(){
+		this.activity.onBackPressed();
+	}
+	
 	private void showArchEntityTabGroup(String uuid, TabGroup tabGroup) {
 		Object archEntityObj = fetchArchEnt(uuid);
 		if(archEntityObj instanceof ArchEntity){
