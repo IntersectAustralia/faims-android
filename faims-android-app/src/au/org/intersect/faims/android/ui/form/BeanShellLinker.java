bd77f42b8848e515b033e452f24722c1bbe834d6
Daniel theodosius <danielt@intersect.org.au>
2013-01-30 12:11:39 +1100

    FAIMS-161 - add user id to be used when generating uuid

--- faims-android-app/src/au/org/intersect/faims/android/ui/form/BeanShellLinker.java
+++ faims-android-app/src/au/org/intersect/faims/android/ui/form/BeanShellLinker.java
@@ -35,6 +35,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import au.org.intersect.faims.android.R;
+import au.org.intersect.faims.android.data.User;
 import au.org.intersect.faims.android.gps.GPSDataManager;
 import au.org.intersect.faims.android.gps.GPSLocation;
 import au.org.intersect.faims.android.managers.DatabaseManager;
@@ -90,6 +91,9 @@ public class BeanShellLinker {
 
 	private MarkerLayer currentPositionLayer;
 	private GPSLocation previousLocation;
+
+	@SuppressWarnings("unused")
+	private User user;
 
 	public BeanShellLinker(FragmentActivity activity, AssetManager assets, UIRenderer renderer, DatabaseManager databaseManager, GPSDataManager gpsDataManager) {
 		this.activity = activity;
@@ -1604,5 +1608,10 @@ public class BeanShellLinker {
 
 	public void setBaseDir(String dir) {
 		this.baseDir = dir;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+		this.databaseManager.setUserId(user.getUserId());
 	}
 }
