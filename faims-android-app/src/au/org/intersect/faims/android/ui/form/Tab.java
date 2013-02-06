f7e7b298144cb34630d7c8eb0c861f7da4a6070d
Daniel theodosius <danielt@intersect.org.au>
2013-02-06 16:21:36 +1100

    FAIMS-18 - Add the data schema to save picture url from vocab to database

--- faims-android-app/src/au/org/intersect/faims/android/ui/form/Tab.java
+++ faims-android-app/src/au/org/intersect/faims/android/ui/form/Tab.java
@@ -26,7 +26,6 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
