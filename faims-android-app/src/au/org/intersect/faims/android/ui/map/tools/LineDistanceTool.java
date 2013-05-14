c61b0a14d732da43a60de8ac3ab7a45d1c6c3f23
Theeban Soundararajan <theeban@intersect.org.au>
2013-05-14 16:29:12 +1000

    FAIMS-265: adding missing files to last commit

---
+++ faims-android-app/src/au/org/intersect/faims/android/ui/map/tools/LineDistanceTool.java
@@ -0,0 +1,246 @@
+package au.org.intersect.faims.android.ui.map.tools;
+
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.location.Location;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import au.org.intersect.faims.android.log.FLog;
+import au.org.intersect.faims.android.nutiteq.CustomLine;
+import au.org.intersect.faims.android.nutiteq.GeometryUtil;
+import au.org.intersect.faims.android.ui.form.MapButton;
+import au.org.intersect.faims.android.ui.map.CustomMapView;
+import au.org.intersect.faims.android.util.MeasurementUtil;
+import au.org.intersect.faims.android.util.ScaleUtil;
+
+import com.nutiteq.components.MapPos;
+import com.nutiteq.geometry.Geometry;
+import com.nutiteq.geometry.VectorElement;
+
+public class LineDistanceTool extends SelectTool {
+	
+	private class LineDistanceToolCanvas extends ToolCanvas {
+		
+		private float textX;
+		private float textY;
+		
+		private boolean hasDistance;
+		private float distance;
+		
+		private boolean showKm;
+		
+		public LineDistanceToolCanvas(Context context) {
+			super(context);
+		}
+
+		@Override
+		public void onDraw(Canvas canvas) {
+			if (hasDistance) {
+				
+				if (showKm) {
+					canvas.drawText(MeasurementUtil.displayAsKiloMeters(distance/1000), textX, textY, textPaint);
+				} else {
+					canvas.drawText(MeasurementUtil.displayAsMeters(distance), textX, textY, textPaint);
+				}
+				
+			}
+		}
+		
+		@Override
+		public void clear() {
+			hasDistance = false;
+			invalidate();
+		}
+
+		public void drawDistance(CustomLine line) {
+			this.hasDistance = true;
+			this.distance = LineDistanceTool.this.computeDistance(line.getVertexList());
+			
+			float offset = ScaleUtil.getDip(this.getContext(), DEFAULT_OFFSET);
+			
+			MapPos p = GeometryUtil.transformVertex(line.getVertexList().get(line.getVertexList().size()-1), LineDistanceTool.this.mapView, true);
+			
+			textX = (float) p.x + offset;
+			textY = (float) p.y + offset;
+			
+			invalidate();
+		}
+		
+		public void setShowKm(boolean value) {
+			showKm = value;
+			invalidate();
+		}
+		
+	}
+	
+	public static final String NAME = "Line Distance";
+	
+	private LineDistanceToolCanvas canvas;
+
+	public LineDistanceTool(Context context, CustomMapView mapView) {
+		super(context, mapView, NAME);
+		canvas = new LineDistanceToolCanvas(context);
+		container.addView(canvas);
+	}
+
+	@Override 
+	public void activate() {
+		super.activate();
+		canvas.clear();
+	}
+	
+	@Override
+	public void deactivate() {
+		super.deactivate();
+		canvas.clear();
+	}
+	
+	@Override
+	public void onLayersChanged() {
+		super.onLayersChanged();
+		canvas.clear();
+	}
+	
+	@Override
+	public void onMapChanged() {
+		super.onMapChanged();
+		drawDistance();
+	}
+	
+	@Override
+	protected void updateLayout() {
+		super.updateLayout();
+		if (canvas != null) layout.addView(canvas);
+	}
+	
+	@Override
+	protected void clearSelection() {
+		super.clearSelection();
+		canvas.clear();
+	}
+	
+	@Override
+	public void onVectorElementClicked(VectorElement element, double arg1,
+			double arg2, boolean arg3) {
+		if (element instanceof Geometry) {
+			try {
+				if ((element instanceof CustomLine) && (mapView.getSelection().size() < 1)) {
+					CustomLine p = (CustomLine) element;
+					
+					if (mapView.hasSelection(p)) {
+						mapView.removeSelection(p);
+					} else {
+						mapView.addSelection(p);
+					}
+					
+					drawDistance();
+				}
+			} catch (Exception e) {
+				FLog.e("error selecting element", e);
+				showError(e.getMessage());
+			}
+		} else {
+			// ignore
+		}
+	}
+	
+	private void drawDistance() {
+		if (mapView.getSelection().size() < 1) return;
+		
+		CustomLine line = (CustomLine) mapView.getSelection().get(0);
+		
+		canvas.drawDistance(line);
+		canvas.setColor(mapView.getDrawViewColor());
+		canvas.setStrokeSize(mapView.getDrawViewStrokeStyle());
+		canvas.setTextSize(mapView.getDrawViewTextSize());
+		canvas.setShowKm(mapView.showKm());
+	}
+	
+	public float computeDistance(List<MapPos> points) {
+		float totalDistance = 0;
+		MapPos lp = null;
+		for (MapPos p : points) {
+			p = GeometryUtil.convertToWgs84(p);
+			if (lp != null) {
+				float[] results = new float[3];
+				Location.distanceBetween(lp.y, lp.x, p.y, p.x, results);
+				totalDistance += results[0];
+			}
+			lp = p;
+		}
+		return totalDistance;
+	}
+	
+	@Override
+	protected MapButton createSettingsButton(final Context context) {
+		MapButton button = new MapButton(context);
+		button.setText("Style Tool");
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+				builder.setTitle("Style Settings");
+				
+				LinearLayout layout = new LinearLayout(context);
+				layout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+				layout.setOrientation(LinearLayout.VERTICAL);
+				
+				final EditText colorSetter = addSetter(context, layout, "Select Color:", Integer.toHexString(mapView.getDrawViewColor()));
+				final SeekBar strokeSizeBar = addSlider(context, layout, "Stroke Size:", mapView.getDrawViewStrokeStyle());
+				final SeekBar textSizeBar = addSlider(context, layout, "Text Size:", mapView.getDrawViewTextSize());
+				final CheckBox decimalBox = addCheckBox(context, layout, "Show Degrees:", !mapView.showDecimal());
+				final CheckBox kmBox = addCheckBox(context, layout, "Show Km:", mapView.showKm());
+				
+				builder.setView(layout);
+				
+				builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						try {
+							int color = parseColor(colorSetter.getText().toString());
+							float strokeSize = parseSize(strokeSizeBar.getProgress());
+							float textSize = parseSize(textSizeBar.getProgress());
+							boolean showDecimal = !decimalBox.isChecked();
+							boolean showKm = kmBox.isChecked();
+							
+							mapView.setDrawViewColor(color);
+							mapView.setDrawViewStrokeStyle(strokeSize);
+							mapView.setDrawViewTextSize(textSize);
+							mapView.setEditViewTextSize(textSize);
+							mapView.setShowDecimal(showDecimal);
+							mapView.setShowKm(showKm);
+							
+							LineDistanceTool.this.drawDistance();
+						} catch (Exception e) {
+							showError(e.getMessage());
+						}
+					}
+				});
+				
+				builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// ignore
+					}
+				});
+				
+				builder.create().show();
+			}
+				
+		});
+		return button;
+	}
+	
+}
