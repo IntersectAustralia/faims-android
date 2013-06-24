c299afbca1579c7450982c3f1d015d81be548685
Theeban Soundararajan <theeban@intersect.org.au>
2013-06-24 16:23:54 +1000

    FAIMS-361: adding missing files to last commit

---
+++ faims-android-app/src/au/org/intersect/faims/android/ui/map/tools/LoadTool.java
@@ -0,0 +1,31 @@
+package au.org.intersect.faims.android.ui.map.tools;
+
+import com.nutiteq.geometry.Geometry;
+import com.nutiteq.geometry.VectorElement;
+
+import android.content.Context;
+import au.org.intersect.faims.android.nutiteq.GeometryData;
+import au.org.intersect.faims.android.ui.map.CustomMapView;
+
+public class LoadTool extends MapTool {
+	
+	public static final String NAME = "Load";
+
+	public LoadTool(Context context, CustomMapView mapView, String name) {
+		super(context, mapView, NAME);
+	}
+	
+	public void onVectorElementClicked(VectorElement element, double arg1,
+			double arg2, boolean arg3) {
+		if (element instanceof Geometry) {
+			Geometry geom = (Geometry) element;
+			if (geom.userData instanceof GeometryData) {
+				GeometryData geomData = (GeometryData) geom.userData;
+				if (geomData.id != null) {
+					mapView.notifyGeometryLoaded(geom);
+				}
+			} 
+		}
+	}
+
+}
